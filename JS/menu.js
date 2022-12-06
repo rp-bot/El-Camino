62892e0fac049bd923c3492b57cd23b4e61434d7
rp-bot_win10 <legacy.business18@gmail.com>
2022-12-05 21:15:25 -0500

    -p

--- JS/menu.js
+++ JS/menu.js
@@ -1,39 +1,94 @@
-let foodContainer = document.querySelector('.food-container')
+let foodContainer = document.querySelector(".food-container");
 
 const footitem = [
-    {
-        FoodName: "Chicken and Rice Bake",
-        foodimg: 'img/Chicken and rice bake.jpeg',
-        price: "13.88",
-        type: "Entree",
-        des: "Mexican chicken and rice, Mexican chicken and rice."
-    },
-    {
-        FoodName: "Chilli con Canre",
-        foodimg: 'img/chilli_con_carne.jpeg',
-        price: "13.88",
-        type: "Entree",
-        des: "Served  over rice and with some corn chips for a hearty, warming meal full of Mexican spices."
-    },
-    {
-        FoodName: "Family Nachos Tray Bake",
-        foodimg: 'img/FamilyNachosTrayBake.jpeg',
-        price: "13.88",
-        type: "Entree",
-        des: "Very cheesy, this Nachos Tray bake is perfect for sharing."
-    },
-    {
-        FoodName: "Pulled Pork Tacos",
-        foodimg: 'img/Pulled Pork Tacos.jpeg',
-        price: "13.88",
-        type: "Entree",
-        des: "Mexican pulled pork tacos Made with a blend of Mexican flavour taco seasonings. Superb taco sauce that flavours the slow-cooked pork perfectly. Topped with melted Perfect Italiano Mexican Cheese and enjoy."
-    },
-    {
-        FoodName: "Taquitos",
-        foodimg: 'img/Taquitos.jpeg',
-        price: "13.88",
-        type: "Entree",
-        des: "Mexican pulled pork tacos Made with a blend of Mexican flavour taco seasonings. Superb taco sauce that flavours the slow-cooked pork perfectly. Topped with melted Perfect Italiano Mexican Cheese and enjoy."
-    }
-]
+	{
+		FoodName: "Chicken and Rice Bake",
+		foodimg: "img/Chicken and rice bake.jpeg",
+		price: "13.88",
+		type: "Entree",
+		des: "Mexican chicken and rice, Mexican chicken and rice.",
+	},
+	{
+		FoodName: "Chilli con Canre",
+		foodimg: "img/chilli_con_carne.jpeg",
+		price: "13.88",
+		type: "Entree",
+		des: "Served  over rice and with some corn chips for a hearty, warming meal full of Mexican spices.",
+	},
+	{
+		FoodName: "Family Nachos Tray Bake",
+		foodimg: "img/FamilyNachosTrayBake.jpeg",
+		price: "13.88",
+		type: "Entree",
+		des: "Very cheesy, this Nachos Tray bake is perfect for sharing.",
+	},
+	{
+		FoodName: "Pulled Pork Tacos",
+		foodimg: "img/Pulled Pork Tacos.jpeg",
+		price: "13.88",
+		type: "Entree",
+		des: "Mexican pulled pork tacos Made with a blend of Mexican flavour taco seasonings. Superb taco sauce that flavours the slow-cooked pork perfectly. Topped with melted Perfect Italiano Mexican Cheese and enjoy.",
+	},
+	{
+		FoodName: "Taquitos",
+		foodimg: "img/Taquitos.jpeg",
+		price: "13.88",
+		type: "Entree",
+		des: "Mexican pulled pork tacos Made with a blend of Mexican flavour taco seasonings. Superb taco sauce that flavours the slow-cooked pork perfectly. Topped with melted Perfect Italiano Mexican Cheese and enjoy.",
+	},
+];
+
+const dessert = [
+	{
+		FoodName: "Churros",
+		foodimg: "img/churros.jpg",
+		price: "13.88",
+		type: "Dessert",
+		des: "Deep-fried dough coated in cinnamon sugar and dipped in chocolate or caramel",
+	},
+	{
+		FoodName: "Fried Ice Cream",
+		foodimg: "img/fried ice cream.jpg",
+		price: "13.88",
+		type: "Dessert",
+		des: "Vanilla ice cream is already glorious, but when you fry it, All that crunch and flavor oh-so-dynamite!",
+	},
+	{
+		FoodName: "Dulce de Leche",
+		foodimg: "img/dulce-de-leche.jpg",
+		price: "13.88",
+		type: "Dessert",
+		des: "Dulce de leche is sweetened milk, and it is phenomenal. It's a lot like caramel, but much better!",
+	},
+	{
+		FoodName: "Caramel Flan",
+		foodimg: "img/Caramel-Flan.jpg",
+		price: "13.88",
+		type: "Dessert",
+		des: "Sweet and melt-in-you-mouth custard oozing with caramel sauce",
+	},
+];
+
+const drink = [
+	{
+		FoodName: "Horchata",
+		foodimg: "img/horchata.jpg",
+		price: "13.88",
+		type: "Drink",
+		des: "Horchata, or orxata, is a name given to various beverages, which are generally plant-based, but sometimes contain animal milk.",
+	},
+	{
+		FoodName: "Lemonade",
+		foodimg: "img/Lemonade.jpg",
+		price: "13.88",
+		type: "Drink",
+		des: "Lemon drink with salt",
+	},
+	{
+		FoodName: "Soda",
+		foodimg: "img/Sodas.jpg",
+		price: "13.88",
+		type: "Drink",
+		des: "A choice of 4 different sodas",
+	},
+];
