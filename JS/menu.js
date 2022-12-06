0490c4bd694e28a08615a7ec9a2907d3cec3e850
Galilea Garcia <ggarcia87@ivytech.edu>
2022-12-05 20:38:04 -0500

    menu.js -g

---
+++ JS/menu.js
@@ -0,0 +1,39 @@
+let foodContainer = document.querySelector('.food-container')
+
+const footitem = [
+    {
+        FoodName: "Chicken and Rice Bake",
+        foodimg: 'img/Chicken and rice bake.jpeg',
+        price: "13.88",
+        type: "Entree",
+        des: "Mexican chicken and rice, Mexican chicken and rice."
+    },
+    {
+        FoodName: "Chilli con Canre",
+        foodimg: 'img/chilli_con_carne.jpeg',
+        price: "13.88",
+        type: "Entree",
+        des: "Served  over rice and with some corn chips for a hearty, warming meal full of Mexican spices."
+    },
+    {
+        FoodName: "Family Nachos Tray Bake",
+        foodimg: 'img/FamilyNachosTrayBake.jpeg',
+        price: "13.88",
+        type: "Entree",
+        des: "Very cheesy, this Nachos Tray bake is perfect for sharing."
+    },
+    {
+        FoodName: "Pulled Pork Tacos",
+        foodimg: 'img/Pulled Pork Tacos.jpeg',
+        price: "13.88",
+        type: "Entree",
+        des: "Mexican pulled pork tacos Made with a blend of Mexican flavour taco seasonings. Superb taco sauce that flavours the slow-cooked pork perfectly. Topped with melted Perfect Italiano Mexican Cheese and enjoy."
+    },
+    {
+        FoodName: "Taquitos",
+        foodimg: 'img/Taquitos.jpeg',
+        price: "13.88",
+        type: "Entree",
+        des: "Mexican pulled pork tacos Made with a blend of Mexican flavour taco seasonings. Superb taco sauce that flavours the slow-cooked pork perfectly. Topped with melted Perfect Italiano Mexican Cheese and enjoy."
+    }
+]
